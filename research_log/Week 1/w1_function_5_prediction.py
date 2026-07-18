5dfa26f2a354e4b7913fc82c5564c76ee3c5165f
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-18 13:56:49 +0100

    Update and rename Function 5 - First Query Point Prediction to w1_function_5_prediction.py

--- research_log/Week 1/w1_function_5_prediction.py
+++ research_log/Week 1/w1_function_5_prediction.py
@@ -7,35 +7,34 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 from scipy.stats.qmc import Sobol
 
-#Loading the data
+# Loading the data
 X = data["function_5"]["x"]      #shape (20, 4)
 Y = data["function_5"]["y"]      #shape (20, )
 
-#Defining and fitting the Gaussian Process (GP) model
-kernel = RBF(length_scale=[0.1, 0.1, 0.1, 0.1]) #aggresive baseline
-#using Automatic Relevance Determination (ARD) with agressive baseline and default length_scale_bounds= (1e-5, 1e5)
+# Defining and fitting the Gaussian Process (GP) model
+kernel = RBF(length_scale=[0.1, 0.1, 0.1, 0.1]) #Aggresive baseline; Using Automatic Relevance Determination (ARD) with agressive baseline and default length_scale_bounds= (1e-5, 1e5)
 gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-10)
 gp.fit(X, Y)
 
-#Defining the input bounds
+# Defining the input bounds
 minimum = X.min(axis=0)
 maximum = X.max(axis=0)
 
-#Sobol sequence (power of 2)
+# Sobol sequence (power of 2)
 sobol = Sobol(d=4, scramble=True)
 unit_samples = sobol.random_base2(m=13) 
 
-#Scale Sobol samples to actual bounds
+# Scale Sobol samples to actual bounds
 x_grid = minimum + unit_samples * (maximum - minimum)
 
-#Computing the Gaussian Process (GP) posterior
+# Computing the Gaussian Process (GP) posterior
 post_mean, post_std = gp.predict(x_grid, return_std=True)
 
-#Computing the Upper Confidence Bound (UCB) acquisition function
+# Computing the Upper Confidence Bound (UCB) acquisition function
 beta = 1.96 #balancing exploitation with exploration
 ucb_acquisition_function = post_mean + beta * post_std
 
-#Next query => argmax acquisition
+# Next query => argmax acquisition
 x_next = x_grid[np.argmax(ucb_acquisition_function)]
 
 print("Next raw query point for Function 5:", x_next)
