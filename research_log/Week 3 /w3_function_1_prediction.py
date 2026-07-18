2534f2f83fb6906dddf8cadc3ca4c53b94893abb
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-18 14:06:51 +0100

    Update and rename research_log/Week 3 /Code for Submitted Queries - Week 3/Function 1 - Third Query Point Prediction to research_log/Week 3 /w3_function_1_prediction.py

--- research_log/Week 3 /w3_function_1_prediction.py
+++ research_log/Week 3 /w3_function_1_prediction.py
@@ -9,35 +9,35 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
 from scipy.stats.qmc import Sobol
 
-#Loading the data
+# Loading the data
 X = data["function_1"]["x"]      # shape (12, 2)
 Y = data["function_1"]["y"]      # shape (12, )
 
-#Defining and fitting the Gaussian Process (GP) model
+# Defining and fitting the Gaussian Process (GP) model
 kernel = Matern(length_scale=[0.1, 0.1], nu=1.5, length_scale_bounds=(1e-6, 1e8)) #ARD, aggresive baseline and wider bounds
 #using a Matern kernel (nu=1.5) as this is a spiky function, with applied Automatic Relevance Determination (ARD) with aggresive baseline and wider bounds 
 gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-4, normalize_y=True)
 gp.fit(X, Y)
 
-#Defining the input bounds
+# Defining the input bounds
 minimum = X.min(axis=0)
 maximum = X.max(axis=0)
 
-#Sobol sequence (power of 2)
+# Sobol sequence (power of 2)
 sobol = Sobol(d=2, scramble=True)
 unit_samples = sobol.random_base2(m=12)
 
-#Scaling Sobol samples to actual bounds
+# Scaling Sobol samples to actual bounds
 x_grid = minimum + unit_samples * (maximum - minimum)
 
-#GP posterior mean + covariance (required for TS)
+# GP posterior mean + covariance (required for TS)
 post_mean, post_cov = gp.predict(x_grid, return_cov=True)
 
-#Thompson Sampling
-#Draw one sample from the GP posterior over all Sobol points
+# Thompson Sampling
+# Draw one sample from the GP posterior over all Sobol points
 sampled_function = np.random.multivariate_normal(post_mean, post_cov)
 
-#Next query => argmax acquisition
+# Next query => argmax acquisition
 x_next = x_grid[np.argmax(sampled_function)]
 
 print("Third raw query point for Function 1:", x_next)
