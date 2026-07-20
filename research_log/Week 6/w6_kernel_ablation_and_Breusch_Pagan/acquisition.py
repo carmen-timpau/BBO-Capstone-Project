29d4eae8c5cba6eae74f3b695a280e37ea922567
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-20 21:55:20 +0100

    Create acquisition.py

---
+++ research_log/Week 6/w6_kernel_ablation_and_Breusch_Pagan/acquisition.py
@@ -0,0 +1,23 @@
+# Acquisition Functions (Designed for Batch/Vector Inputs)
+# Tested in Acquisition Ablation Study
+
+def compute_acquisition_scores(X_cand, gp, y_best, acq_type, param=0.01):
+    """Computes acquisition scores for a given GP and candidate points."""
+    mu, sigma = gp.predict(X_cand, return_std=True)
+    sigma = np.maximum(sigma, 1e-9)
+    
+    if acq_type == "EI":
+        # param = xi (exploration trade-off)
+        improvement = mu - y_best - param
+        Z = improvement / sigma
+        return improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+
+    elif acq_type == "UCB":
+        # param = beta (confidence multiplier)
+        return mu + param * sigma
+
+    elif acq_type == "PI":
+        # param = xi
+        improvement = mu - y_best - param
+        Z = improvement / sigma
+        return norm.cdf(Z)
