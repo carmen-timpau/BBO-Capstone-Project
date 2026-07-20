ee838de0adf117d41e1909c5f20c2bb1a2a1df77
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-20 21:56:25 +0100

    Update w6_acquisition_ablation_study.py

--- research_log/Week 6/w6_kernel_ablation_and_Breusch_Pagan/w6_acquisition_ablation_study.py
+++ research_log/Week 6/w6_kernel_ablation_and_Breusch_Pagan/w6_acquisition_ablation_study.py
@@ -10,31 +10,10 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.exceptions import ConvergenceWarning
 
 from kernels import get_kernel_suite, get_kernel_suite_f1
+from acquisition import compute_acquisition_scores
 
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 warnings.filterwarnings("ignore", message="Predicted variances smaller than 0", category=UserWarning,)
-
-# Acquisition Functions (Designed for Batch/Vector Inputs)
-def compute_acquisition_scores(X_cand, gp, y_best, acq_type, param=0.01):
-    """Computes acquisition scores for a given GP and candidate points."""
-    mu, sigma = gp.predict(X_cand, return_std=True)
-    sigma = np.maximum(sigma, 1e-9)
-    
-    if acq_type == "EI":
-        # param = xi (exploration trade-off)
-        improvement = mu - y_best - param
-        Z = improvement / sigma
-        return improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
-
-    elif acq_type == "UCB":
-        # param = beta (confidence multiplier)
-        return mu + param * sigma
-
-    elif acq_type == "PI":
-        # param = xi
-        improvement = mu - y_best - param
-        Z = improvement / sigma
-        return norm.cdf(Z)
 
 # LOOCV Acquisition Function Ablation Study
 acq_ablation_summary = {}
