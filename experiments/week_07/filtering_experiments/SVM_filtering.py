a800bd03aa72164c1ac275e67a09c99ba18569a8
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-25 07:37:28 +0100

    Update SVM_filtering.py

--- experiments/week_07/filtering_experiments/SVM_filtering.py
+++ experiments/week_07/filtering_experiments/SVM_filtering.py
@@ -1,7 +1,6 @@
-"""Data Quality Control: SVM Filtering for Bayesian Optimization (Offline Experiment)
+"""Data Quality Control: SVM Filtering for Bayesian Optimization
 
-Training an SVM classifier on Week 6 input data to establish a boundary between 
-high-quality and low-quality data points. This offline evaluation was conducted 
-while awaiting Week 6 query results to better inform Week 7 query selection via 
-Bayesian Optimization.
+Training an SVM classifier on Week 7 input data to establish a boundary between 
+high-quality and low-quality data points. This evaluation was conducted to potentially 
+better inform query selection via Bayesian Optimization.
 """
