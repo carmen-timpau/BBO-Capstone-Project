a08b5d4b944d7b367dc55c92fea53ae2b94873a7
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-25 07:36:24 +0100

    Update NN_surrogate.py

--- experiments/week_07/filtering_experiments/NN_surrogate.py
+++ experiments/week_07/filtering_experiments/NN_surrogate.py
@@ -1,6 +1,6 @@
-"""Surrogate Modeling: Neural Network Evaluation for Bayesian Optimization (Offline Experiment)
+"""Surrogate Modeling: Neural Network Evaluation for Bayesian Optimization
 
 Training and evaluating a Neural Network for Black-Box function surrogate modelling in Bayesian Optimisation. 
-This offline experiment was conducted on Week 6 input data while awaiting query results to assess model accuracy 
-and performance before potential deployment in Week 7's Bayesian Optimization loop for next query prediction.
+This experiment was conducted on Week 7 input data to assess model accuracy and performance before potential 
+deployment in Bayesian Optimization loop for next query prediction.
 """
