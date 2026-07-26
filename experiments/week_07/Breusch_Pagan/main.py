a541f1cbd1a0418aa2c3ea811f4d1e334ee0a870
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 00:30:19 +0100

    Update main.py

--- experiments/week_07/Breusch_Pagan/main.py
+++ experiments/week_07/Breusch_Pagan/main.py
@@ -1,1 +1,19 @@
+import pickle
+from kernel_ablation import run_kernel_ablation
+from Breusch_Pagan import run_bp
 
+def main():
+    print("Loading Week 7 full input data snapshot...") # All data collected so far till Week 7 (initial data + 6 submitted queries)
+    with open("wk7_input_data.pkl", "rb") as file:
+        data = pickle.load(file) 
+
+    print("\n[STEP 1] Running Kernel Ablation Study...")
+    top_kernels = run_kernel_ablation(data)
+
+    print("\n[STEP 2] Running Breusch-Pagan Homo/Heteroscedasticity Test...")
+    run_bp(data, top_kernels)
+
+    print("\nWeek 7 Breusch Pagan Homo/Heteroscedasticity Test Pipeline execution complete!")
+
+if __name__ == "__main__":
+    main()
