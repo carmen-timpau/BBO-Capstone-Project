713a9be3cb383bfd6ce67ccb4e21c49ae6b6176c
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 21:14:10 +0100

    Update acquisition.py

--- experiments/week_07/acquisition_ablation/acquisition.py
+++ experiments/week_07/acquisition_ablation/acquisition.py
@@ -4,25 +4,21 @@
 import numpy as np
 from scipy.stats import norm
 
-def compute_acquisition_scores(X_cand, gp, y_best, acq_type, param=0.01):
-    """Computes acquisition scores for a given GP and candidate points."""
-    mu, sigma = gp.predict(X_cand, return_std=True)
+def compute_acquisition_scores(X_candidates, gp, y_best, acq_type, param):
+    """Computes rigorous acquisition values using GP posterior mean and std."""
+    mu, sigma = gp.predict(X_candidates, return_std=True)
     sigma = np.maximum(sigma, 1e-9)
     
-    if acq_type == "EI":
-        # param = xi (exploration trade-off)
+    if acq_type.upper() == "EI":
         improvement = mu - y_best - param
-        Z = improvement / sigma
-        return improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
-
-    elif acq_type == "UCB":
-        # param = beta (confidence multiplier)
+        z = improvement / sigma
+        ei = improvement * norm.cdf(z) + sigma * norm.pdf(z)
+        ei[sigma == 0.0] = 0.0
+        return ei
+    elif acq_type.upper() == "UCB":
         return mu + param * sigma
-
-    elif acq_type == "PI":
-        # param = xi
-        improvement = mu - y_best - param
-        Z = improvement / sigma
-        return norm.cdf(Z)
+    elif acq_type.upper() == "PI":
+        z = (mu - y_best - param) / sigma
+        return norm.cdf(z)
     else:
         raise ValueError(f"Unknown acquisition type: {acq_type}")
