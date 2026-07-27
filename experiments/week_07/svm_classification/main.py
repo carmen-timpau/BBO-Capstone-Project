76e3839c3393a1a0ed7fcbd86039fa07e5cbf3b9
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 01:04:50 +0100

    Update main.py

--- experiments/week_07/svm_classification/main.py
+++ experiments/week_07/svm_classification/main.py
@@ -1,11 +1,17 @@
+import sys
+import os
 import pickle
-from kernel_ablation import run_kernel_ablation
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from kernel_ablation.kernel_ablation import run_kernel_ablation
 from svm_classification import run_svm_classification
 
 def main():
     print("Loading Week 7 full input data snapshot...") # All data collected so far till Week 7 (initial data + 6 submitted queries)
-    with open("wk7_input_data.pkl", "rb") as file:
-        data = pickle.load(file) 
+    data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'wk7_input_data.pkl'))
+    with open(data_path, "rb") as file:
+        data = pickle.load(file)
 
     print("\n[STEP 1] Running Kernel Ablation Study...")
     top_kernels = run_kernel_ablation(data)
