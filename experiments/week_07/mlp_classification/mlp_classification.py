08df2953f9767fe52916989c41e5042366db3d7e
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 12:32:04 +0100

    Update mlp_classification.py

--- experiments/week_07/mlp_classification/mlp_classification.py
+++ experiments/week_07/mlp_classification/mlp_classification.py
@@ -3,5 +3,5 @@
 Using Multilayer Perceptrons (MLPs) to classify the outputs of the 8 small-dataset Black-Box functions.
 
 The classification performance of MLPs will be compared with that of NuSVMs for each function, using 
-the ROC-AUC (Receiver Operating Characteristic - Area Under Curve) performance metric.
+the 5-fold Stratified Cross-Validation ROC-AUC (Receiver Operating Characteristic - Area Under Curve) performance metric.
 """
