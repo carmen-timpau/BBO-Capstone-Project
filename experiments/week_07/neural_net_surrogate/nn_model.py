521fa1f13f584f7a34f81e5db58dee9834281ca1
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 12:16:00 +0100

    Update nn_model.py

--- experiments/week_07/neural_net_surrogate/nn_model.py
+++ experiments/week_07/neural_net_surrogate/nn_model.py
@@ -1,6 +1,6 @@
 """Surrogate Modeling: Evaluation of Deep Ensembles of Neural Networks for Bayesian Optimization
 
 Training and evaluating Deep Ensembles of Neural Network for Black-Box function surrogate modelling in Bayesian Optimisation. 
-This experiment was conducted on Week 7 input data to assess model accuracy and performance before potential 
-deployment in Bayesian Optimization loop for next query prediction.
+This experiment was conducted on Week 7 input data to assess model accuracy and performance before potential deployment in 
+Bayesian Optimization loop for next query prediction.
 """
