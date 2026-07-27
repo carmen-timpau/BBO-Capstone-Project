7552bcec59ee3ee78afbb43206a35e7d4283bbdb
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 12:13:06 +0100

    Update mlp_classification.py

--- experiments/week_07/mlp_classification/mlp_classification.py
+++ experiments/week_07/mlp_classification/mlp_classification.py
@@ -1,1 +1,3 @@
-
+# Using Neural Networks (MLPs) to classify the outputs of the small-dataset Black-Box Functions
+# Their classification performance will be compared with NuSVMs, 
+# using the ROC-AUC (Receiver Operating Characteristic - Area Under Curve) performance metric
