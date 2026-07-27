e339a06878568f12feea34088f7ae5c7fd33e0d1
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 16:22:47 +0100

    Update gaussian_process.py

--- experiments/week_07/gaussian_process/gaussian_process.py
+++ experiments/week_07/gaussian_process/gaussian_process.py
@@ -8,7 +8,7 @@ def evaluate_gaussian_process(X_train, y_train, X_test, best_kernel):
         kernel=best_kernel,
         alpha=0.0,
         normalize_y=True,
-        n_restarts_optimizer=10,  # Increased restarts for better hyperparameter optimization
+        n_restarts_optimizer=10,
         random_state=42
     )
     gp.fit(X_train, y_train)
