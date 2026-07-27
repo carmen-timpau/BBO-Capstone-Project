0eb99735f8c1a00c12ba2c47f96b95365a9f7aac
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-27 12:18:58 +0100

    Update nn_model.py

--- experiments/week_07/neural_net_surrogate/nn_model.py
+++ experiments/week_07/neural_net_surrogate/nn_model.py
@@ -3,4 +3,7 @@
 Training and evaluating Deep Ensembles of Neural Network for Black-Box function surrogate modelling in Bayesian Optimisation. 
 This experiment was conducted on Week 7 input data to assess model accuracy and performance before potential deployment in 
 Bayesian Optimization loop for next query prediction.
+
+The performance metric for surrogate modeling that will be used to compare Deep Ensembles of Neural Networks with GPs as 
+surrogate models for the Bayesian Optimisation of the 8 Black-Box functions will be Root Mean Squared Error (RMSE).
 """
