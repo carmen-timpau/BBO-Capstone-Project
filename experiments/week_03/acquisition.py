f0219e90ca857b36f9a4ddba5c46b39c4b963ecf
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-07-30 16:37:34 +0100

    Update acquisition.py

--- experiments/week_03/acquisition.py
+++ experiments/week_03/acquisition.py
@@ -1,1 +1,16 @@
+# Functions computing the acquisition function score
 
+import numpy as np
+from scipy.stats import norm
+
+def upper_confidence_bound(
+    mean: np.ndarray, std: np.ndarray, beta: float = 1.96
+) -> np.ndarray:
+    """Calculating Upper Confidence Bound (UCB) balancing exploitation and exploration"""
+    return mean + beta * std
+
+def thompson_sampling(
+    mean: np.ndarray, cov: np.ndarray
+) -> np.ndarray:
+    """Calculating Thompson Sampling (TS) draw from GP posterior sample"""
+    return np.random.multivariate_normal(mean, cov)
