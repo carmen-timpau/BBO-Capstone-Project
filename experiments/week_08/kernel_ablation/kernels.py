4b8a88277568c6d06782dbbb613c7487adb97e1e
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-08-02 00:52:04 +0100

    Update kernels.py

--- experiments/week_08/kernel_ablation/kernels.py
+++ experiments/week_08/kernel_ablation/kernels.py
@@ -38,6 +38,6 @@ def get_kernel_suite(n_dims, ls_bounds=(1e-2, 1e4), noise_bounds=(1e-6, 1e2)):
         )
     }
 
-def get_kernel_suite_f1(n_dims, ls_bounds=(1e-3, 1e8), noise_bounds=(1e-10, 1e2)):
+def get_kernel_suite_f1(n_dims, ls_bounds=(5e-3, 1e8), noise_bounds=(1e-10, 1e2)):
     """Wrapper function that passes Function 1 specific bounds into the main suite."""
     return get_kernel_suite(n_dims, ls_bounds=ls_bounds, noise_bounds=noise_bounds)
