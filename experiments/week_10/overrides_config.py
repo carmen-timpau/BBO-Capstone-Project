eac4de923e618fca2a26478be2d8e18dbad60103
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-08-22 09:08:16 +0100

    Create overrides_config.py

---
+++ experiments/week_10/overrides_config.py
@@ -0,0 +1,33 @@
+# Config file with function-specific overrides used in Week 10 BBO Pipeline
+# Configs identified in pre-production, during manual pipeline troubleshooting, evaluation and tuning
+
+n_init_base_overrides={
+            "function_1": 4, 
+            "function_2": 4
+        }
+
+init_per_dim_overrides={
+            "function_4": 1, 
+            "function_5": 1, 
+            "function_6": 1,
+            "function_7": 1, 
+            "function_8": 1
+        }
+
+holdout_fraction_overrides={
+            "function_1": 0.6,
+            "function_2": 0.6,
+            "function_5": 0.15,
+            "function_8": 0.15
+        }
+
+n_seeds_overrides={
+            "function_1": 1000, 
+            "function_2": 1000,
+            "function_3": 500, 
+            "function_4": 500, 
+            "function_5": 500,
+            "function_6": 500, 
+            "function_7": 500, 
+            "function_8": 500
+        }
