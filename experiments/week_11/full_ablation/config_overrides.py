eee313d3f52f575f4bb547e0cb9b9201cc46ab0d
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-08-23 14:49:09 +0100

    Update config_overrides.py

--- experiments/week_11/full_ablation/config_overrides.py
+++ experiments/week_11/full_ablation/config_overrides.py
@@ -1,1 +1,36 @@
+# Configurations file with global defaults and function-specific overrides used in Week 11 Full Ablation
+# Overrides were identified in pre-production, during manual pipeline evaluation and troubleshooting
+# More details on this can be found in week_10/strategy_summary.md, as these were set up in Week 10 of the BBO capstone project
 
+# Global Defaults
+n_init_base = 5
+init_per_dim = 2
+n_iterations = 15
+n_seeds = 500
+holdout_fraction = 0.3
+
+# Function-specific Overrides
+n_init_base_overrides={
+            "function_1": 4, 
+            "function_2": 4
+        }
+
+init_per_dim_overrides={
+            "function_4": 1, 
+            "function_5": 1, 
+            "function_6": 1,
+            "function_7": 1, 
+            "function_8": 1
+        }
+
+holdout_fraction_overrides={
+            "function_1": 0.6,
+            "function_2": 0.6,
+            "function_5": 0.15,
+            "function_8": 0.15
+        }
+
+n_seeds_overrides={
+            "function_1": 1000, 
+            "function_2": 1000
+        }
