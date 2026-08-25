181bc73e9a2dbe088afd20f66a578d8748bffceb
Carmen-Mihaela Timpau <carmen.timpau21@alumni.imperial.ac.uk>
2026-08-25 16:46:29 +0100

    Update acq_strategies.py

--- experiments/week_13/full_ablation/acq_strategies.py
+++ experiments/week_13/full_ablation/acq_strategies.py
@@ -1,5 +1,5 @@
-"""Centralized acquisition function configurations for a highly-exploitative kernel x acqusition joint ablation, 
-part of the Week 13 BBO Bayesian Optimisation ML pipeline (for predicting the final set of next queries in this project)."""
+"""Centralized acquisition function configurations for a highly-exploitative kernel x acqusition joint ablation, part of 
+the Week 13 BBO Bayesian Optimisation ML pipeline (for predicting the final set of next queries in this project, 1 per function)."""
 
 acq_strategies = {
     "Expected Improvement (xi=0.001)": ("EI", 0.001),
